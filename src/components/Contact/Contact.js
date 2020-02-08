8e35916efdf7650c561379ff3e8e5c1928e01919
AzamatKaynarov <kaynarov2909@mail.ru>
2020-02-08 11:19:37 +0500

    Тернарный оператор

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,18 @@
 import React from 'react'
 
 export default function Contact(props) {
-    let status="";
-    if (props.online===true) {
-        status="greenBtn";
-    }else{
-        status="redBtn";
-    }
+    
+    const good = props.online ? 'greenButton' : 'redButton';
     
 
     return (
         <div className="main">
           <div className="photo"><img src={props.ava} alt=""/>
-          <div className={status}></div>
+          <div className={good}></div>
           </div>
           <div className="content">
               <p className="name">{props.name}</p>
-              <p className="message">{props.lastM}</p>
+              <p className="message">{props.lastMessage}</p>
               <p className="tag">{props.tag}</p>
           </div>
             
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,14 +3,18 @@ import Contacts from '../Contact/Contact'
 
 export default function ContactList() {
     const contacts = [
-        { name: "Alex", online: true, ava: "https://i.pravatar.cc/150?img=14", tag: ["#friendly", "#cool", "#dude"], lastM: "I'm happy!g" },
-        { name: "Marina", online: false, ava: "https://i.pravatar.cc/150?img=26" , tag: ["#beautiful", "#lucky", "#girlfriend"], lastM: "Wher are you?" },
-        { name: "Adam", online: true, ava: "https://i.pravatar.cc/150?img=60", tag: ["#nigga", "#dude", "#brother"], lastM: "What do you say?" },
-        { name: "Sabrina", online: false, ava: "https://i.pravatar.cc/150?img=36", tag: ["#friend", "#happy"], lastM: "What do you doing?" },
+        { name: "Alex", online: true, ava: "https://i.pravatar.cc/150?img=14", tag: ["#friendly", "#cool", "#dude"], lastMessage: "I'm happy!g" },
+        { name: "Marina", online: false, ava: "https://i.pravatar.cc/150?img=26" , tag: ["#beautiful", "#lucky", "#girlfriend"], lastMessage: "Wher are you?" },
+        { name: "Adam", online: true, ava: "https://i.pravatar.cc/150?img=60", tag: ["#nigga", "#dude", "#brother"], lastMessage: "What do you say?" },
+        { name: "Sabrina", online: false, ava: "https://i.pravatar.cc/150?img=36", tag: ["#friend", "#happy"], lastMessage: "What do you doing?" },
     ]
+    var mapped = contacts.map(function(tag) {
+        return tag;
+    })
     return (
         <div>
-            {contacts.map(o => <Contacts name={o.name} ava={o.ava} tag={o.tag} lastM={o.lastM} online={o.online} />)}
+
+            {contacts.map(o => <Contacts name={o.name} ava={o.ava} tag={o.tag} lastMessage={o.lastMessage} online={o.online} />)}
         </div>
     )
 }
